dcb0bfbf1a165138b3bbc066b16c05a04f0bb67a
koli899 <tusharkoli899@gmail.com>
2025-07-26 10:48:39 +0530

    cors origin add

--- backend/SkillGate/server.js
+++ backend/SkillGate/server.js
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-  origin: "https://skill-gate-chi.vercel.app",
+  origin: "https://skill-gate-alpha.vercel.app",
   origin: "http://localhost:5173",  
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   credentials: true
